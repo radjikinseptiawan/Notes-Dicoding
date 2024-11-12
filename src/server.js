594581dfccafaa81e7693a8146eb25e08cf229a9
radjikinseptiawan <radjikin21@gmail.com>
2024-11-13 04:56:08 +0700

    create web server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
+const Hapi = require('@hapi/hapi');
+
+const init = async () =>{
+  const server = Hapi.server({
+    port : 5000,
+    host : 'localhost'
+  });
+  await server.start();
+  console.log(`server berjalan di ${server.info.uri}`);
+};
+
+init();
